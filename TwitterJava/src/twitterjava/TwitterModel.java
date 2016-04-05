6f9edb6b4e538dbd1193309fccd198c1805c7851
Kurniawan <Kurniawan@Kurniawan>
2016-04-06 02:56:11 +0700

    updated

---
+++ TwitterJava/src/twitterjava/TwitterModel.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twitterjava;
+
+import twitter4j.*;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Kurniawan
+ */
+public class TwitterModel {
+
+    private Twitter twitter;
+
+    public TwitterModel() {
+        twitter = new TwitterFactory().getInstance();
+    }
+
+    public void tweetStatus(String tweet) {
+        try {
+            twitter.updateStatus(tweet);
+            JOptionPane.showMessageDialog(null, "Pesan Berhasil");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "judul exception",
+                    "pesan exception", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public String getHomeTimeLine() {
+        String s = "";
+        try {
+            List<Status> status = twitter.getHomeTimeline();
+            for (int i = 0; i < status.size(); i++) {
+                s = s + status.get(i).getUser().getScreenName() + " - "
+                        + status.get(i).getText() + "\n\n";
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "judul exception",
+                    "pesan exception", JOptionPane.ERROR_MESSAGE);
+        }
+        return s;
+    }
+    public void followUser(String user){
+        try {
+            twitter.createFriendship(user);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "judul exception",
+                    "pesan exception", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
