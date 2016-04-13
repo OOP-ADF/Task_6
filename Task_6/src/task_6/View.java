25399e1bae8fdce8d9d9dabdd45a5596c940b485
ASUS PC <ASUS PC@A455LN>
2016-04-13 23:12:21 +0700

    ANNISA RIZKI PRATIWI WIBOWO/1301144391/IF-38-01

---
+++ Task_6/src/task_6/View.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task_6;
+
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author ASUS PC
+ */
+public interface View {
+    public void addListener(ActionListener e);
+}
