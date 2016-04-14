951563ad71039b0a72bfa6a6801c59ec5bf27983
harizmhd <harizmhd@gmail.com>
2016-04-14 11:04:38 +0700

    Finished Task_6

---
+++ JavaTwitter/src/controller/Driver.java
@@ -0,0 +1,12 @@
+package controller;
+
+/**
+ * @author Muhammad Hariz Arasy
+ * 1301140259
+ * IF-38-09
+ */
+public class Driver {
+    public static void main(String[] args) {
+        Controller control = new Controller();
+    }
+}
