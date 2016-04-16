0c8a35ae5f8ae001f066c8ec9ee7c2c7345fb33a
fannyshaf <nurshafarani8@gmail.com>
2016-04-17 02:04:26 +0700

    [TASK_6] Fatima Nurshafarani_1301144245_IF-38-01

---
+++ Twitter4J/src/twitter/Twitter.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twitter;
+
+import java.util.List;
+import twitter4j.Status;
+
+/**
+ *
+ * @author Fatima Nurshafarani
+ */
+class Twitter {
+
+    void updateStatus(String tweet) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    List<Status> getHomeTimeline() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void createFriendship(String user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
