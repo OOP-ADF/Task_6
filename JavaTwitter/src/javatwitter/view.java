3078a3695d07d69ba171468ad2bd16b71ea7f89a
ekaputeri22 <eka.puteri77@yahoo.com>
2016-05-01 13:53:49 +0700

    EkaPutri_1301144312_IF3802
    EkaPutri_1301144312_IF3802[Task 6 Selesai]

---
+++ JavaTwitter/src/javatwitter/view.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatwitter;
+
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Ekaaaa
+ * NIM 1301144312
+ * Kelas IF - 38 - 02
+ */
+public interface view {
+    public void addListener(ActionListener e);
+}
