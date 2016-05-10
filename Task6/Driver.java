95abf2c495cd586ecd0b4464cb802493029513db
irghifari <irghifari@gmail.com>
2016-05-10 23:01:12 +0700

    Add files via upload

---
+++ Task6/Driver.java
@@ -0,0 +1,12 @@
+ 
+  package twitter;
+  
+  /**
+   *
+   * @author IchsanGhifari
+   */
+  public class Driver {
+      public static void main(String[] args) {
+          Controller control = new Controller();
+      }
+  }
---
+++ Task6/View.java
@@ -0,0 +1,12 @@
+ 
+  package twitter;
+  
+  import java.awt.event.ActionListener;
+  
+  /**
+   *
+   * @author IchsanGhifari
+   */
+  public interface View {
+      public void addListener(ActionListener e);
+  }
