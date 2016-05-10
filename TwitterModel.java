94033e9adbc56cc14fa48358fc6dae872c960023
Gctatmaja <genturcta@gmail.com>
2016-05-11 00:25:29 +0700

    Task_6

---
+++ TwitterModel.java
@@ -0,0 +1,67 @@
+package Task_6;
+  
+ 
+import twitter4j.*;
+import java.util.List;
+import javax.swing.JOptionPane;
+  
+/**
+*
+* @author Gentur Cipto Tri Atmaja
+* IF 38 09 / 1301140239
+*/
+
+public class TwitterModel 
+{
+    private Twitter twitter;
+      
+    public TwitterModel()
+	{
+		twitter = new TwitterFactory().getInstance();
+    }
+    
+	public void tweetStatus (String tweet)
+	{
+        try
+		{
+            twitter.updateStatus(tweet);
+            JOptionPane.showMessageDialog(null, "pesan berhasil");
+        } 
+		catch (Exception e)
+		{
+               JOptionPane.showMessageDialog(null, "judul exception","pesan exception", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+	public String getHomeTimeline()
+	{
+        try 
+		{
+            String s = "";
+            List<Status> status = twitter.getHomeTimeline();
+            for (int i = 0; i < status.size(); i  ) {
+                s = s   status.get(i).getUser().getScreenName()   " - "
+                    status.get(i).getText()   "\n\n";
+        }
+            return s;
+    
+		catch (Exception e) 
+		{
+            JOptionPane.showMessageDialog(null,"Pesan Error","Error", JOptionPane.ERROR_MESSAGE);
+            return null;
+        }    
+	}
+   
+    public void followUser(String user)
+	{
+        try
+		{
+            twitter.createFriendship(user);
+        }
+		
+		catch (Exception e)
+		{
+            JOptionPane.showMessageDialog(null, "judul exception", "pesan exception", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+} 
---
+++ View.java
@@ -0,0 +1,14 @@
+package Task_6;
+  
+import java.awt.event.ActionListener;
+  
+/**
+*
+* @author Gentur Cipto Tri Atmaja
+* IF 38 09 / 1301140239
+*/
+  
+public interface View 
+{
+    public void addListener(ActionListener e);
+} 
