02cd3476efbe8265f5dbf4d9d75ee24e026f1c9c
Hafidrozaq <syahrizalrozaqi@gmail.com>
2016-05-10 23:26:35 +0700

    Done

---
+++ AFoRtweet/src/afortweet/AFoRtweet.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afortweet;
+
+/**
+ *
+ * @author HAFID
+ */
+public class AFoRtweet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Controller c = new Controller();
+    }
+    
+}
