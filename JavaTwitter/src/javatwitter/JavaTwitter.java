e0333b99b17cfd63ebb7d931689eeabded4d523d
Alim Nuriqra <noeralim21@gmail.com>
2016-05-11 23:40:30 +0700

    Alim Nuriqra A 1301144032

---
+++ JavaTwitter/src/javatwitter/JavaTwitter.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatwitter;
+
+/**
+ *
+ * @author SD
+ */
+public class JavaTwitter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
