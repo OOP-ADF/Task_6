8ad36d2e77ba4b4eb06a9e0cd16c03fb63e96775
praanggi09 <praanggi09@gmail.com>
2017-05-18 14:23:16 +0700

    Anggi Pratama Nasution_1301154259_IF3907
    done

---
+++ src/Task_6/View.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Task_6;
+
+import java.awt.event.ActionListener;
+
+
+
+/**
+ *
+ * @author praanggi
+ */
+public interface View {
+    
+    public void addListener(ActionListener e);
+    
+}
